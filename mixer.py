cdb52d6ea02fd8a3f1d1043c7a2ef86f653404e7
SALVATOR <bigratteam@gmail.com>
2024-12-28 10:13:58 -0800

    main file

---
+++ mixer.py
@@ -0,0 +1,63 @@
+import random
+from art import text2art
+from colorama import Fore
+parts = [
+    "£minik",
+    "11.11.C4.12.12",
+    "¥fttks-122334",
+    "darkweb_000_tor",
+    "Virus.ly",
+    "viru3",
+    "gk-999.2.5.4-php",
+    "VIRUSphp",
+    "Null_999999999_tacklorix_8085_tor_000_Fill_Filtr_Filter",
+    "R0B1KA.bug=/bAg=/Bag=/BuG.=/bUg.FYLLTER",
+    "rest+5.106.8.151,M12,bug-3.5.4-ping,xxx+10.10.34.35",
+    "b0Y0a2cafbaf668e282d2dc02a1fe2a7,Spam%100%2Filter.channel.after.change:x",
+    "x=%d,y=%d,x,y",
+    "bug-3.5.4-badg-000-Bag-999",
+    "M24.gk.C4.ufttk-000,fata",
+    "¥fttk-8085-python.bUg",
+    "ping-error+106.404.151",
+    "Error:-:404:-:rubika.ir",
+    "H.N.Z.7H9",
+    "(/#filtered-фильтроваться-筛选-φιλτραριστεί/)",
+    "/H/A.U.M.R*/(xxx)z/+4.4.3.5.0.4\"fil",
+    "python=/phishing-web-rubika.ir(3.11.6-1)",
+    "bUg</R0Bika>reset_4000_polpy.filtering.filtr.fil.virus.85k.akant.anti-islam.gk",
+    "bug/%12233-4.dev/#2323-24.BaG",
+    "60bdffd3a2be4h",
+    "code-frame/-/code-frame-7.0.0.tgz",
+    "hmmhdNmmmNNMmdssshNdy",
+    "Viru3:-:Error402",
+    "Filter-Filter0_filter000virus000hack000pore/fil.تایم تور.yfttk_01k.in",
+    "noboteubikasupoeraplekshenrubikaserver/www.filtering.rubika.polpy",
+    "reset_22.24.93.220:8000_anti-SupportBot",
+    "Akbaknkknnkq.Filter.hacking.web-rubika",
+    "terminal_تایم تور_termux.hacking-activity.inrubika.0x0.0x0.0x0",
+    "Akbaknkknnkq",
+    "(/Akbaknkknnkq***$Fil/)",
+    "yftt⅕ķ<Filter>ýfțţ⁹⁹k",
+    "Badge:-:Terr.flf/4r4dc9ekcdk.Thviru3.oiu/35.208.137.69,56,84/GHor6.7.hfdjsh.34.36",
+    "teramport,208,91,197,27,9721a1cd79583ddd9ae744fb7b507d5d.30",
+    "noboteubikasupoeraplekshenrubikaserver",
+    "ter.am.fil.ter.af/f7hh82l35k/CT=81.28.49.29/bot_filtr.php-fil",
+    "tramport/#sxs.fil{h/g}php=sex-link)362±7120",
+    "reset_99977_polpy/server.offline.onlline"
+]
+print(Fore.RED+text2art('SALVATOR'))
+links = [ "https://imgurl.ir/uploads/z9951_InShot_20240224_115729792.jpg", "https://s8.uupload.ir/files/img_20240302_021538_421_5ey.jpg", "https://s8.uupload.ir/files/img_20240302_022230_985_tt52.jpg", "https://imgurl.ir/uploads/i32304_IMG_20240302_125006_833.jpg", "https://imgurl.ir/uploads/a037047_IMG_20240302_125006_953.jpg", "https://imgurl.ir/uploads/g787946_IMG_20240302_125006_103.jpg", "https://s8.uupload.ir/files/images_(3)_4lkb.jpeg", "https://imgurl.ir/uploads/u73287_VID_20240302_170745_435.mp4", "https://imgurl.ir/uploads/j825317_VID_20240302_135748_932.mp4", "https://imgurl.ir/uploads/f477651_VID_20240302_140807_052.mp4", "https://imgurl.ir/uploads/y076030_VID_20240302_140632_131.mp4", "https://imgurl.ir/uploads/q280660_VID_20240302_134920_183.mp4", "https://imgurl.ir/uploads/c18824_4_5962999256506700423.mp4", "https://imgurl.ir/uploads/d15047_af.png", "https://s4.uupload.ir/files/img_20210728_194736_412_9t6h.jpg", "https://imgtr.ee/images/2023/10/17/a07b7180a3a1d43b490fd7a62c8e7d11.png", "https://s8.uupload.ir/files/(2)_zk05.jpg", "https://s8.uupload.ir/files/_video_al6f.jpg", "https://imgurl.ir/uploads/k5289_darkweb.png", "https://s8.uupload.ir/files/img_20230527_223527_960_224m.jpg", "https://s8.uupload.ir/files/crdghzedz1lx_6ew0.jpg", "https://s8.uupload.ir/files/screenshot_%DB%B2%DB%B0%DB%B2%DB%B3%DB%B0%DB%B5%DB%B0%DB%B4-%DB%B1%DB%B6%DB%B2%DB%B6%DB%B5%DB%B5_chrome_yxag.jpg", "https://uploadkon.ir/uploads/c33111_23IMG-20230420-183307-124.jpg", "https://cdn.itsup.com/creatives/41/300442pie4k4.jpg", "https://s8.uupload.ir/files/20230613_130653_xe3r.jpg", "https://uploadkon.ir/uploads/4ddf16_23InShot-%DB%B2%DB%B0%DB%B2%DB%B3%DB%B0%DB%B4%DB%B1%DB%B7-%DB%B0%DB%B2%DB%B4%DB%B7%DB%B2%DB%B2%DB%B3%DB%B4%DB%B8.jpg", "https://uploadkon.ir/uploads/c33111_23IMG-20230420-183307-124.jpg", "https://uploadkon.ir/uploads/08ec03_23InShot-20231101-222651376.jpg", "https://up.20script.ir/file/2f7c-InShot-%DB%B2%DB%B0%DB%B2%DB%B3%DB%B0%DB%B9%DB%B1%DB%B3-%DB%B1%DB%B9%DB%B4%DB%B9%DB%B5%DB%B۸%DB%B۹%DB%B۸%DB%B۷.jpg", "https://imgurl.ir/uploads/o17116_-ografi-XNXX-Rubika-xXx.gif" ]
+print(Fore.LIGHTBLUE_EX+'1-get a mix of profane words\n2-get a sex link\n3-exit')
+choice = input('enter your choice: ')
+
+if choice == '2':
+    ran=random.choice(links)
+    print(Fore.GREEN+f'your link is: {ran}')
+elif choice == '1':
+    random_items = random.sample(parts, 10)
+
+    formatted_items = "/>".join([f"[{item}]" for item in random_items])
+
+    print(Fore.YELLOW+formatted_items)
+elif choice == '3':
+    pass
